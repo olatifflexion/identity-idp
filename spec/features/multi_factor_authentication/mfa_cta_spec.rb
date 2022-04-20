dbea2649e9921caf57effa46e55bc6987a3516a1
Jessica Dembe <jessica.dembe@gsa.gov>
2022-04-20 14:39:10 -0400

    move test from back_up_code_sign_up_spec to mfa_cta_spec

---
+++ spec/features/multi_factor_authentication/mfa_cta_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+feature 'mfa cta banner' do
+	include DocAuthHelper
+	include SamlAuthHelper
+  
+
+	context 'multiple factor authentication feature is disabled' do
+    it 'does not display a banner' do
+      visit_idp_from_sp_with_ial1(:oidc)
+      user = sign_up_and_set_password
+      select_2fa_option('backup_code')
+      click_continue
+
+      expect(MfaPolicy.new(user).multiple_factors_enabled?).to eq false
+      expect(page).to have_current_path(sign_up_completed_path)
+      expect(page).not_to have_content(t('mfa.second_method_warning.text'))
+    end
+  end
+
+  context 'multiple factor authentication feature is enabled' do
+    before do
+      allow(IdentityConfig.store).to receive(:select_multiple_mfa_options).and_return(true)
+    end
+
+    it 'displays a banner' do
+      visit_idp_from_sp_with_ial1(:oidc)
+      user = sign_up_and_set_password
+      select_2fa_option('backup_code')
+      click_continue
+
+      expect(page).to have_current_path(sign_up_completed_path)
+      expect(MfaPolicy.new(user).multiple_factors_enabled?).to eq false
+      expect(page).to have_content(t('mfa.second_method_warning.text'))
+    end
+
+    it 'redirects user to choose multiple methods of authentication when banner is displayed' do
+      visit_idp_from_sp_with_ial1(:oidc)
+      user = sign_up_and_set_password
+      select_2fa_option('backup_code')
+      click_continue
+      click_on(t('mfa.second_method_warning.link'))
+
+      expect(page).to have_current_path(sign_up_completed_path)
+    end
+
+    it 'does not display a banner' do
+      visit_idp_from_sp_with_ial1(:oidc)
+      user = sign_up_and_set_password
+      select_2fa_option('backup_code')
+      click_continue
+    end
+  end    
+end
--- spec/features/two_factor_authentication/backup_code_sign_up_spec.rb
+++ spec/features/two_factor_authentication/backup_code_sign_up_spec.rb
@@ -74,36 +74,6 @@ feature 'sign up with backup code' do
     expect(current_url).to start_with('http://localhost:7654/auth/result')
   end
 
-  context 'multiple factor authentication feature is disabled' do
-    it 'does not display a banner' do
-      visit_idp_from_sp_with_ial1(:oidc)
-      user = sign_up_and_set_password
-      select_2fa_option('backup_code')
-      click_continue
-
-      expect(MfaPolicy.new(user).multiple_factors_enabled?).to eq false
-      expect(page).to have_current_path(sign_up_completed_path)
-      expect(page).not_to have_content(t('mfa.second_method_warning.text'))
-    end
-  end
-
-  context 'multiple factor authentication feature is enabled' do
-    before do
-      allow(IdentityConfig.store).to receive(:select_multiple_mfa_options).and_return(true)
-    end
-
-    it 'displays a banner' do
-      visit_idp_from_sp_with_ial1(:oidc)
-      user = sign_up_and_set_password
-      select_2fa_option('backup_code', 'phone')
-      click_continue
-
-      expect(page).to have_current_path(sign_up_completed_path)
-      expect(MfaPolicy.new(user).multiple_factors_enabled?).to eq false
-      expect(page).to have_content(t('mfa.second_method_warning.text'))
-    end
-  end
-
   def sign_out_user
     first(:link, t('links.sign_out')).click
   end
